b9b003d77c67d9e809c6f06685098ce9f0ebb1be
cubillos1395 <54371574+cubillos1395@users.noreply.github.com>
2022-12-18 23:14:22 -0500

    Add files via upload

---
+++ src/API/ProductosAPI.js
@@ -0,0 +1,39 @@
+// let url = 'http://localhost:5000/'; 
+// url += 'productos/'; 
+
+const url = 'http://localhost:5000/productos/';
+
+export async function listarProductos(){
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.productos;
+}
+
+export async function crearProducto(producto){
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(producto)
+    });
+    const data = await res.json();
+    return data;
+}
+
+export async function actualizarProducto(producto){
+    const res = await fetch(url, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(producto)
+    });
+    const data = await res.json();
+    return data;
+}
+
+export async function eliminarProducto(id){
+    const res = await fetch(url + `${id}`,{
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    });
+    const data = await res.json();
+    return data;
+}
